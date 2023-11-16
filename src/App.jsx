1aad8950113238c2b9b2f68e05b9492ae346496c
fahimagithub <fahima96khan@gmail.com>
2023-11-17 02:46:06 +1100

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Header from './components/Header'
 import About from './pages/about'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
@@ -9,6 +10,7 @@ import SignUp from './pages/SignUp'
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import {FaSearch} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className='bg-red-100 shadow-md '>
+        <div className='flex justify-between items-center max-w-6xl mx-auto 
+        p-3'>
+            <Link to="/">
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-red-800'>Real</span>
+                    <span className='text-red-600'>Estate</span>
+                </h1>
+            </Link>
+            <form className='bg-red-50 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder='Search...' 
+                className='bg-transparent focus:outline-none
+                w-24 sm:w-64'/>
+                <FaSearch className='text-slate-500'/>
+            </form>
+            <ul className='flex gap-4 text-red-800'>
+                <Link to='/'>
+                    <li className='hidden sm:inline hover:underline cursor-pointer'>
+                        Home
+                    </li>
+                </Link>
+                <Link to='/about'>
+                    <li className='hidden sm:inline hover:underline cursor-pointer'>
+                        About
+                    </li>
+                </Link>
+                <Link to='/sign-in'>
+                    <li className='hover:underline cursor-pointer'>
+                        SignIn
+                    </li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
