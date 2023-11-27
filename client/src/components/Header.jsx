9f825b990021a6ff3754bcac1cb69275428aeaa5
fahimagithub <fahima96khan@gmail.com>
2023-11-27 21:52:49 +1100

    complete sign-up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className='bg-red-100 shadow-md '>
+    <header className='shadow-md '>
         <div className='flex justify-between items-center max-w-6xl mx-auto 
         p-3'>
             <Link to="/">
