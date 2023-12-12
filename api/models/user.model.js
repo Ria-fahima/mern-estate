f0d9f517b536a695123ee0f816fc24cabfc8df03
fahimagithub <fahima96khan@gmail.com>
2023-12-12 16:48:24 +1100

    Add Google OAuth functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2019/08/11/18/59/icon-4399701_960_720.png"
+    },
 
 }, {timestamps: true});
 
