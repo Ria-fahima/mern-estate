3d18a4e70741cd08ea5c614bdbbbf9c7b83c91f3
fahimagithub <fahima96khan@gmail.com>
2024-02-01 14:41:06 +1100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,43 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-red-700'>About Real Estate</h1>
+      <p className='mb-4 text-slate-800'>
+        Real Estate stands out as a premier real estate agency, 
+        specializing in assisting clients in the purchase, sale, 
+        and rental of properties within the most desirable 
+        neighborhoods. Our seasoned team of experienced agents is 
+        devoted to ensuring a seamless and exceptional experience 
+        throughout the buying and selling process. At Real Estate, 
+        we take pride in our commitment to delivering top-notch 
+        service and making the journey of acquiring or selling a
+        property as smooth as possible.
+       </p>
+       <p className="mb-4 text-slate-800">
+          Our mission at Real Estate is clear: to help our clients
+          achieve their real estate aspirations by offering expert
+          advice, personalized service, and an in-depth 
+          understanding of the local market. Whether you're in 
+          the market to buy, sell, or rent a property, our 
+          dedicated team is here to guide you at every step,
+          providing the support and expertise needed to make 
+          informed decisions. Real Estate is not just a real 
+          estate agency; we are your partners in navigating the 
+          dynamic real estate landscape, ensuring your goals 
+          are met with success.
+       </p>
+       <p className="mb-4 text-slate-800">
+        With a wealth of experience and knowledge in the real 
+        estate industry, our team of agents at Real Estate is 
+        committed to delivering the highest level of service to 
+        our clients. We firmly believe that the process of buying
+        or selling a property should be an exciting and rewarding
+        experience. Real Estate is dedicated to making this vision
+         a reality for each and every client we serve, leveraging 
+         our expertise and commitment to excellence in the 
+         ever-evolving real estate market.
+       </p>
+    </div>
   )
 }
